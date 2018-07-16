dbc020645c97b5c8989630533cdb2ec7900981d6
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-16 14:14:45 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,9 +1,11 @@
 # Code your cash register here!
 class AmazonTransaction
 attr_accessor :total, :items, :discount
-def initialize(discount=20)
+def initialize(total, discount=20, items)
   @total = 0
   @items = []
   @discount
 end
 end
+
+initialize()
